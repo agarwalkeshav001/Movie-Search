54f2ed1d4b49a0130135e349cb5e35e712103407
Keshav Agarwal <keshavagrwl123@gmail.com>
2022-01-11 00:03:40 +0530

    update app.js

--- app.js
+++ app.js
@@ -29,64 +29,6 @@ var express = require("express"),
         
     });
 
-app.post('/pay',(req,res) =>{
-    var name = req.body.name;
-    var email = req.body.email;
-    var purpose = req.body.purpose;
-    var amount = req.body.amount;
-//     var data = new Insta.PaymentData();
- 
-//     data.purpose = purpose            // REQUIRED
-//     data.amount = amount                  // REQUIRED
-//     data.email = email
-//     data.buyer_name   = name
-//     data.currency = 'INR';
-//     data.phone = 9674561602;
-//     data.send_sms= true;
-//     data.send_email= true;
-//     data.redirect_url= 'http://www.google.com';
-    
-    
-//     Insta.createPayment(data, function(error, response) {
-//     if (error) {
-//         console.log(error);
-//     } else {
-//         console.log(response);
-//     }
-//     });
-    var headers = { 'X-Api-Key': 'e6803a9ac4edcb6e61ef95bd0aa61427', 'X-Auth-Token': 'ce5969c8bf1e754f966d19adb29e2c08'}
-    var payload = {
-            purpose: purpose,
-            amount: amount,
-            buyer_name: name,
-            redirect_url: 'http://moviess-reviewss.herokuapp.com/form',
-            send_email: true,
-            email: email,
-            allow_repeated_payments: false
-    }
-    console.log(payload);
-    axios.post('https://www.instamojo.com/api/1.1/payment-requests/', {form: payload,  headers: headers}).then (function(response, body){
-    if(response.statusCode == 201){
-    console.log(body);
-  }
-})
-        .catch(function (error) {
-    console.log(error);
-  });
-})
-
-app.post('/getid',(req,res)=>{
-        
-        var buyer= req.body.email;
-        var status= req.body.status;
-        var id = req.body.payment_id;
-        console.log(buyer,status,id);
-        console.log(req.body);
-        res.send(buyer,status);
-
-        
-    });
-
     app.get('/index',(req,res)=>{
         res.render('index.ejs');
     })
